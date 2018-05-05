4e1be500368d9c89ce2c30bf4fb87fa3527fa1e4
linnealindahl <lilinda@kth.se>
2018-05-05 18:45:13 +0200

    Add files via upload

--- models/models.py
+++ models/models.py
@@ -1,3 +1,8 @@
+# encoding=utf8
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 import matplotlib.pyplot as plt
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, mean_absolute_error
@@ -7,6 +12,7 @@ from math import sqrt
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
 import warnings
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
@@ -15,70 +21,100 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 from matplotlib import pyplot
 from mpl_toolkits.mplot3d import Axes3D
 
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true).ravel(), np.array(y_pred).ravel()
+    value=0
+    for number in range(len(y_true)):
+        #print(abs(np.take(y_true,number)-np.take(y_pred,number))/(np.take(y_true,number)))
+        value+=abs(np.take(y_true,number)-np.take(y_pred,number))/(np.take(y_true,number))
+    answer=(value/len(y_true))*100
 
-def cross_vali(csv_file,y_header,x_header, number_of_splits,titel):
+    # print("Y_TRUE: ", y_true)
+    # print("Y_pred: ", y_pred)
+
+    #return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+    return answer
+
+
+
+def cross_vali(csv_file,y_header,x_header, number_of_splits,titel, y_label,x_label):
+    #read data from file and normalize it
     df=pd.read_csv(csv_file)
     df = df[df[x_header] != 0]
+    max_y=max(df['revenue'])
+    print(max_y)
     scaler = MinMaxScaler()
     scaled_values = scaler.fit_transform(df)
     df.loc[:,:] = scaled_values
-    #df = df.sort_values(by=x_header, ascending=False)
-    print(df)
+
     y=df[y_header]
     y=y.reshape(len(y),1)
+    y=y.ravel()
 
     x = df[x_header]
-    x=x.reshape(len(x),1)
+    X=x.reshape(len(x),1)
 
 
     #split the trainingdata and testdata
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+    #x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+    X, x_test, y, y_test = train_test_split(X, y, test_size = 0.2)
 
-    #create linear regression object
-    regr = KNeighborsRegressor(2, weights='distance')
-    regr = linear_model.LinearRegression()
-    error =  []
-    coeficient = 0
-    MAE = 0
-    MSE = 0
-    Root_MSE = 0
-    r2 = 0
+    # Fit regression model
+    #regr_1 = DecisionTreeRegressor(max_depth=3, min_samples_leaf=5)
+    regr_1 = linear_model.LinearRegression()
+    regr_2 = RandomForestRegressor(max_depth=3)
+    regr_3 = KNeighborsRegressor(3, weights='distance')
+    regr_1.fit(X, y)
+    regr_2.fit(X, y)
+    regr_3.fit(X, y)
+    y_1 =regr_1.predict(x_test)
+    y_2 =regr_2.predict(x_test)
+    y_3 =regr_3.predict(x_test)
+    # print(y_test,y_1)
+    print(x_label)
+    print('r2_y1:',r2_score(y_test,y_1))
+    print('r2_y2:',r2_score(y_test,y_2))
+    print('r2_y3:',r2_score(y_test,y_3))
+    print('MAPE_y1:',mean_absolute_percentage_error(y_test, y_1))
+    print('MAPE_y2:',mean_absolute_percentage_error(y_test, y_2))
+    print('MAPE_y3:',mean_absolute_percentage_error(y_test, y_3))
+    #
+    # print("y_test ", y_test)
+    # print("Y_1: ", y_1)
 
-    # train the model using the training sets
-    regr.fit(x_train,y_train)
-    # make predictions using the testing sets
-    prediction = regr.predict(x_test)
-    #coeficient = regr.coef_
-    MAE = mean_absolute_error(y_test, prediction)
-    MSE = mean_squared_error(y_test, prediction)
-    Root_MSE = np.sqrt(mean_squared_error(y_test, prediction))
-    r2 = r2_score(y_test, prediction)
-
+    # Predict
     X_grid = np.arange(min(x_test), max(x_test), 0.1)
+    X_grid = X_grid.reshape((len(X_grid), 1))
+    y_1 = regr_1.predict(X_grid)
+    y_2 = regr_2.predict(X_grid)
+    y_3 = regr_3.predict(X_grid)
 
-    # The coefficients
-    #print('Coefficients:', coeficient)
-    #the mean squared error
-    print('Mean Absolute Error:', MAE)
-    print('Mean Squared Error:', MSE)
-    print('Root Mean Squared Error:', Root_MSE)
-    print("r2_score:", r2)
-
-
-    # plot outputs
-    plt.scatter(x_test,y_test, color='black')
-    plt.plot(x_test,prediction,color='red',linewidth=3)
-    #plt.scatter(prediction,color='red')
-    #plt.scatter(y_test,color='blue')
-    #print("P: ", y_test)
+    # Plot the results
+    #plt.figure()
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.set_xticks(np.arange(0, 1, 0.1))
+    ax.set_yticks(np.arange(0, 1., 0.1))
+    plt.scatter(x_test, y_test, s=20, edgecolor="black",
+                c="dimgrey", label="Data")
+    #linear regression plot
+    plt.plot(X_grid, y_1,linestyle='-', color="black",
+             label="Linear Regression", linewidth=2)
+    #random forest plot
+    plt.plot(X_grid, y_2,linestyle='--', color="black", label="Random Forest Regression", linewidth=2)
+    #KnN-plot
+    plt.plot(X_grid, y_3, linestyle=':', color="black", label=" K-Nearest Neighbor Regression", linewidth=2)
 
     plt.title(titel)
-    plt.ylabel(y_header)
-    plt.xlabel(x_header)
-
-    plt.xticks(())
-    plt.yticks(())
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
     plt.legend()
+    #plt.show()
+
+    #plt.xticks(())
+    #plt.yticks(())
+
+    plt.grid()
     plt.show()
 
 
@@ -86,10 +122,12 @@ def cross_vali(csv_file,y_header,x_header, number_of_splits,titel):
 
 def main():
     y='revenue'
-    x='production_budget'
-    titel='K-nearest neighbors'
+    y_label='Intäkter'
+    x='actor'
+    x_label='Engelska-Wikipedia Sidvisningar'
+    titel=x_label
     #datasets = movie_object()     #import list with movieobjects
-    cross_vali("new.csv",y,x,2,titel)
+    cross_vali("new.csv",y,x,2,titel, y_label,x_label)
     #linear_regression(x_train,y_train,x_test,y_test,y,x)
 
 if __name__ == '__main__':
